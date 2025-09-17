c5957d09c4c14909eac5fa1c57e350f70c0d93a9
IceNinja2017 <stewiegriffen2017@gmail.com>
2025-09-17 15:26:15 +0800

    fixed the .env loading problem

--- services/payment-service/index.js
+++ services/payment-service/index.js
@@ -1,14 +1,26 @@
 import express from "express";
-import dotenv from "dotenv-flow";
+import dotenvFlow from "dotenv-flow";
+import path from "path";
+import { fileURLToPath } from "url";
+
+  // Recreate __dirname in ESM
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  // Load global .env first (services/.env)
+  dotenvFlow.config({
+    path: path.resolve(__dirname, "../") // points to services/
+  });
+
+  // Load service-specific .env (auth-service, product-service, payment-service)
+  dotenvFlow.config({
+    path: path.resolve(__dirname, ".") // points to current service folder
+  });
 
 const app = express();
-
-//global env
-dotenv.config({path: "../.env"});
-
-//service specific
-dotenv.config();
+const port = process.env.PORT;
+const service_name = process.env.SERVICE_NAME;
 
 app.listen(5001, () =>{
-    console.log("Server started at http://localhost:5001");
-})
+    console.log(service_name + " Server started at http://localhost:" + port);
+});
