7b5968bd12fc45353b94ae0424b1b8638255f59e
IceNinja2017 <stewiegriffen2017@gmail.com>
2025-09-17 13:57:30 +0800

    Changed from dotenv to dotenv-flow

--- services/auth-service/index.js
+++ services/auth-service/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import dotenv from "dotenv-flow";
 
 const app = express();
+
+//global env
+dotenv.config({path: "../.env"});
+
+//service specific
+dotenv.config();
 
 app.listen(5000, () =>{
     console.log("Server started at http://localhost:5000");
