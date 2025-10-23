e9ed4073533d65b93ad20330f6f8ed58c7749810
IceNinja2017 <stewiegriffen2017@gmail.com>
2025-10-23 11:29:57 +0800

    Revert "Added the getUserById endpoint to be used bu other services"
    This reverts commit 6c0eab8843ad256fe8c67f49916de04959b02855.

--- services/auth-service/routes/auth.route.js
+++ services/auth-service/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {login, logout, register, verifyEmail} from "../controllers/auth.contoller.js"
+import { getUserById, login, logout, register, verifyEmail } from "../controllers/auth.contoller.js"
 import { me, validateToken } from "../controllers/verification.controller.js";
 import { authenticate } from "../middleware/authorize.js";
 
 const router = express.Router();
+
+router.get("/:userId", getUserById);
 
 router.post("/register", register);
 
@@ -11,10 +13,10 @@ router.post("/login", login);
 
 router.post("/logout", logout);
 
-router.post("/verify-email", verifyEmail)
+router.post("/verify-email", verifyEmail);
 
 router.get("/me", authenticate, me);
 
-router.get("/validate-token", authenticate, validateToken)
+router.get("/validate-token", authenticate, validateToken);
 
 export default router;
