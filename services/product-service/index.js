9677177a7dee790b6eeaa4dc7b4de182e9fc95ce
Roche <rochejohnkenneth@gmail.com>
2025-11-06 15:04:17 +0800

    feat: add Cloudinary integration for image management and update dependencies

--- services/product-service/index.js
+++ services/product-service/index.js
@@ -3,11 +3,13 @@ import dotenvFlow from "dotenv-flow";
 import { loadEnv } from "../config/loadEnv.js";
 import { connectDB } from "../config/db.js";
 import mongoose from "mongoose";
+import { ConnectCloudinary } from "./middleware/cloudinary.config.js";
 
 loadEnv(import.meta.url, dotenvFlow);
+ConnectCloudinary();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.ProductService_PORT;
 const SERVICE_NAME = process.env.SERVICE_NAME;
 
 app.listen(PORT, () =>{
