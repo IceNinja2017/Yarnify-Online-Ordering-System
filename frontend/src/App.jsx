3dab32db6e16ad7b9090ce195f165acd7b34f1e4
IceNinja2017 <stewiegriffen2017@gmail.com>
2025-11-17 18:36:14 +0800

    feat: add PaymentCancelPage and route to handle payment cancellations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import { Toaster } from "react-hot-toast"
 import ShopPage from "./pages/ShopPage.jsx"
 import OrdersPage from "./pages/OrdersPage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
+import PaymentCancelPage from "./pages/PaymentCancelPage.jsx"
 
 function App() {
   return (
@@ -71,6 +72,7 @@ function App() {
             <Route path="/verify-email" element={<EmailVerificationPage />} />
             <Route path="/register" element={<RegistrationPage />} />
             <Route path="/payment/return" element={<PaymentReturnPage/>} />
+            <Route path="/payment/cancel" element={<PaymentCancelPage/>} />
             <Route path="/shop" element={<ShopPage/>}/>
             <Route path="/orders" element={<OrdersPage/>}/>
             <Route path="/profile" element={<ProfilePage/>}/>
