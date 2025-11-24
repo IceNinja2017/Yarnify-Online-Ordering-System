5ade54f0c5bae085c2a7502120fcedbbaadd6ea8
IceNinja2017 <stewiegriffen2017@gmail.com>
2025-11-24 15:44:11 +0800

    fix: set secure cookie flag to true for all environments

--- services/auth-service/middleware/generateTokenAndSetCookie.js
+++ services/auth-service/middleware/generateTokenAndSetCookie.js
@@ -11,7 +11,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     //cookie setter
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
